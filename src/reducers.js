5c15811e7e06e1946c91eb8afd097abcc5c80c06
lordvalmir <lukas.kopacka@email.cz>
2019-07-07 21:37:04 +0200

    new reducer

---
+++ src/reducers.js
@@ -0,0 +1,14 @@
+export const CHANGE_SEARCHFIELD = 'CHANGE_SEARCHFIELD';
+
+const intialState = {
+	searchField: ''
+}
+
+export const searchRobots = (state=intialState, action={}) => {
+	switch(action.type) {
+		case: CHANGE_SEARCHFIELD:
+			return Object.assign({}, state, searchField: action.payload);
+		default:
+			return state;
+	}
+}
