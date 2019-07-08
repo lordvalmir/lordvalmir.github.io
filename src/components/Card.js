77217ed2c94229194f5ddbf238d9326781bb45f7
lordvalmir <lukas.kopacka@email.cz>
2019-07-08 20:53:41 +0200

    brackets update

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,6 +1,6 @@
 import React from 'react';
 
-const Card = ({name, email, id}) => {
+const Card = ({ name, email, id }) => {
 	return (
 		<div className='tc bg-light-green dib bbr3 mw5 h6 pa3 ma2 grow bw2 shadow-5'> 
 			<img alt='robots' src={`https://robohash.org/${id}?size=250x250`}/>
